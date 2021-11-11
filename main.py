309be47266363a87bcbf25e1d7d96d51d53984e5
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-12 06:59:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from datetime import datetime
 import discord
+import schedule
 from discord.ext import commands
 import music
 import requests
+import time
 import asyncio
 
 client = commands.Bot(command_prefix=">", intents = discord.Intents.all())
@@ -13,22 +15,10 @@ cogs = [music]
 welcome = f"""I'm Online Right Now.
 Author: @iDead"""
 
-pagi = discord.Embed(
-    title="--- AutoSend ---",
-    description="Good Morning, everyone",
-)
-siang = discord.Embed(
-    title="--- AutoSend ---",
-    description="Good Afternoon, friends",
-)
-malam = discord.Embed(
-    title="--- AutoSend ---",
-    description="Good Evening, Bois",
-)
-tengah_malam = discord.Embed(
-    title="--- AutoSend ---",
-    description="Good Night, everyone",
-)
+channel1 = client.get_channel(851806673232199730)
+channel2 = client.get_channel(905017361353035806)
+
+ping1 = str(f"Bot Latency: {round(client.latency * 1000)}ms")
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
@@ -38,25 +28,20 @@ async def on_ready():
     print('''Welcome to Discord Music Player Bot.
 Logged in as {0.user}'''.format(client))
     current_time = datetime.utcnow().strftime("%H:%M")
-    channel1 = client.get_channel(851806673232199730)
-    channel2 = client.get_channel(905017361353035806)
     await channel1.send(welcome)
-    await channel1.send(f"Bot Latency: {round(client.latency * 1000)}ms")
+    await channel1.send(ping1)
     await channel2.send(welcome)
-    await channel2.send(f"Bot Latency: {round(client.latency * 1000)}ms")
+    await channel2.send(ping1)
     
-    if current_time == "07:00":
-        await channel1.send(embed=pagi)
-        await channel2.send(embed=pagi)
-    elif current_time == "11:00":
-        await channel1.send(embed=siang)
-        await channel2.send(embed=siang)
-    elif current_time == "19:00":
-        await channel1.send(embed=malam)
-        await channel2.send(embed=malam)
-    elif current_time == "23:00":
-        await channel1.send(embed=tengah_malam)
-        await channel2.send(embed=tengah_malam)
+    schedule.every().day.at("07:00").do(pagi)
+    schedule.every().day.at("11:30").do(siang)
+    schedule.every().day.at("19:00").do(malam)
+    schedule.every().day.at("23:00").do(tengah_malam1)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 
 @client.command() #ping
 async def ping(ctx):
@@ -104,5 +89,37 @@ async def supported(ctx):
     embed.set_footer(text="Author: {}".format(author), icon_url=ctx.message.author.avatar_url)
     
     await ctx.send(embed=embed)
+    
+def pagi():
+    pagi1 = discord.Embed(
+        title="--- AutoSend ---",
+        description="Good Morning, everyone",
+    )
+    await channel1.send(embed=pagi1)
+    await channel2.send(embed=pagi1)
 
-client.run('OTA0MTU2MDI2ODUxNDU1MDA2.YX3a6w.8Bt_jbhu432HFbMjsc26BM53hjg')
+def siang():
+    siang1 = discord.Embed(
+        title="--- AutoSend ---",
+        description="Good Afternoon, friends",
+    )
+    await channel1.send(embed=siang1)
+    await channel2.send(embed=siang1)
+    
+def malam():
+    malam1 = discord.Embed(
+        title="--- AutoSend ---",
+        description="Good Evening, Bois",
+    )
+    await channel1.send(embed=malam1)
+    await channel2.send(embed=malam1)
+    
+def tengah_malam():
+    tengah_malam1 = discord.Embed(
+        title="--- AutoSend ---",
+        description="Good Night, everyone",
+    )
+    await channel1.send(embed=tengah_malam1)
+    await channel2.send(embed=tengah_malam1)
+
+client.run(os.getenv('TOKEN'))
