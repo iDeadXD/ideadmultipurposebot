f4c0791a8989e9da4fbd8fe30cebb220ba7d6f9a
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-13 07:17:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -82,39 +82,46 @@ async def supported(ctx):
 
 async def reminder():
     await client.wait_until_ready()
-    ch1 = client.get_channel(channel1)
-    ch2 = client.get_channel(channel2)
-    hour = int(datetime.now(pytz.timezone('Asia/Jakarta')).time().strftime("%H"))
-    minute = int(datetime.now(pytz.timezone('Asia/Jakarta')).time().strftime("%M"))
-    if hour == 7:
-        pagi1 = discord.Embed(
-            title="--- AutoSend ---",
-            description="Good Morning, everyone",
-        )
-        await ch1.send(embed=pagi1)
-        await ch2.send(embed=pagi1)
-    if hour == 11 and minute == 30:
-        siang1 = discord.Embed(
-            title="--- AutoSend ---",
-            description="Good Afternoon, friends",
-        )
-        await ch1.send(embed=siang1)
-        await ch2.send(embed=siang1)
-    if hour == 19:
-        malam1 = discord.Embed(
-            title="--- AutoSend ---",
-            description="Good Evening, Bois",
-        )
-        await ch1.send(embed=malam1)
-        await ch2.send(embed=malam1)
-    if hour == 23 and minute == 30:
-        tengah_malam1 = discord.Embed(
-            title="--- AutoSend ---",
-            description="Good Night, everyone",
-        )
-        await ch1.send(embed=tengah_malam1)
-        await ch2.send(embed=tengah_malam1)
-    await asyncio.sleep(30)
+    while not client.is_closed():
+        hour = int(datetime.now(pytz.timezone('Asia/Jakarta')).time().strftime("%H"))
+        minute = int(datetime.now(pytz.timezone('Asia/Jakarta')).time().strftime("%M"))
+        if hour == 07 and minute == 00:
+            ch1 = client.get_channel(851806673232199730)
+            ch2 = client.get_channel(905017361353035806)
+            pagi1 = discord.Embed(
+                title="--- AutoSend ---",
+                description="Good Morning, everyone",
+            )
+            await ch1.send(embed=pagi1)
+            await ch2.send(embed=pagi1)
+        elif hour == 11 and minute == 30:
+            ch1 = client.get_channel(851806673232199730)
+            ch2 = client.get_channel(905017361353035806)
+            siang1 = discord.Embed(
+                title="--- AutoSend ---",
+                description="Good Afternoon, friends",
+            )
+            await ch1.send(embed=siang1)
+            await ch2.send(embed=siang1)
+        elif hour == 19 and minute == 00:
+            ch1 = client.get_channel(851806673232199730)
+            ch2 = client.get_channel(905017361353035806)
+            malam1 = discord.Embed(
+                title="--- AutoSend ---",
+                description="Good Evening, Bois",
+            )
+            await ch1.send(embed=malam1)
+            await ch2.send(embed=malam1)
+        elif hour == 23 and minute == 30:
+            ch1 = client.get_channel(851806673232199730)
+            ch2 = client.get_channel(905017361353035806)
+            tengah_malam1 = discord.Embed(
+                title="--- AutoSend ---",
+                description="Good Night, everyone",
+            )
+            await ch1.send(embed=tengah_malam1)
+            await ch2.send(embed=tengah_malam1)
+        await asyncio.sleep(3600)
 
 client.loop.create_task(reminder())
 client.run('OTA0MTU2MDI2ODUxNDU1MDA2.YX3a6w.8Bt_jbhu432HFbMjsc26BM53hjg')
