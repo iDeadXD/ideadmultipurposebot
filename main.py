47f41018f4c04c45f0733c4058582bd325a6d570
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-14 11:47:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ cogs = [music]
 
 welcome = f"""I'm Online Right Now.
 Author: @iDead
-Bot Latency: {int(round(client.latency * 1000))}ms"""
+Bot Latency: {float(round(client.latency * 1000))}ms"""
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
