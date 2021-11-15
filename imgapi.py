a7ab72e081e0677820be78fd47c945e9554a8b7b
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-15 17:33:42 +0700

    Create imgapi.py

---
+++ imgapi.py
@@ -0,0 +1,7 @@
+API = {
+    "waifu1": "https://api.waifu.pics/sfw/waifu",
+    "slap1": "https://api.waifu.pics/sfw/slap",
+    "hentai1": "https://api.waifu.pics/nsfw/waifu",
+    "hentai2": "https://api.waifu.pics/nsfw/neko",
+    "hentai3": "https://api.waifu.pics/nsfw/blowjob",
+}
