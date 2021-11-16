f74d6ad7ed2b33227e05fb2987aafd7f26fa98b5
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-16 18:10:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ Logged in as {0.user}'''.format(client))
 async def waifu(ctx, member : discord.Member=None):
     """Waifu Image for You"""
     ch3 = client.get_channel(int(CHANNEL['channel3']))
-    if ctx.channel.id is not ch3:
+    if ctx.channel is not ch3:
         await ctx.send("Note: Write this command in {}".format(ch3.mention))
         return
     else:
