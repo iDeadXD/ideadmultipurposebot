cffbf3c00d95a688344c2fada98002c889261212
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-16 07:18:36 +0700

    Update imgapi.py

--- imgapi.py
+++ imgapi.py
@@ -1,6 +1,9 @@
-API = {
+SFW = {
     "waifu1": "https://api.waifu.pics/sfw/waifu",
     "slap1": "https://api.waifu.pics/sfw/slap",
+}
+
+NSFW = {
     "hentai1": "https://api.waifu.pics/nsfw/waifu",
     "hentai2": "https://api.waifu.pics/nsfw/neko",
     "hentai3": "https://api.waifu.pics/nsfw/blowjob",
