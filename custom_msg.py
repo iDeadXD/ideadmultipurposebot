e0355d2c6e45c6fea1c48f2e0b865d8403070243
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-16 17:34:48 +0700

    Create custom_msg.py

---
+++ custom_msg.py
@@ -0,0 +1,42 @@
+#Waifu Message
+W_MESSAGE = {
+    "w_msg1": "Do you love me, {}?",
+    "w_msg2": "I love you, {}",
+    "w_msg3": "Do you love her, {}?",
+    "w_msg4": "Do you want her to be your girlfriend, {}?",
+    "w_msg5": "Please make me your bride, {}",
+    "w_msg6": "Take her to dinner, {author}",
+    "w_msg7": "I will be your girlfriend, {}",
+    "w_msg8": "Hey, {}. Can you give me some love?",
+    "w_msg9": "Hey {}, you're my boyfriend right?",
+    "w_msg10": "She's your girlfriend right, {}?",
+}
+
+#Hentai Message
+H_MESSAGE = {
+    "h_msg1": "Are you satisfied, {}",
+    "h_msg2": "You're horny right?, {}",
+    "h_msg3": "I think, you're pervert {}",
+    "h_msg4": "You got a good one, {}"
+}
+
+#Bonk Message
+B_MESSAGE = {
+    "b_msg1": "{} bonk to {}",
+    "b_msg2": "{}: I'll bonk you, {}",
+    "b_msg3": "BONK!!!!, {}"
+}
+
+#Slap Message
+S_MESSAGE = {
+    "s_msg1": "{} slapped {}",
+    "s_msg2": "{}: I got you, {}",
+}
+
+#Meme Message
+M_MESSAGE = {
+    "m_msg1": "Laugh for today, {}!! (If the meme is funny)",
+    "m_msg2": "Memes for you today, {}",
+    "m_msg3": "Is this funny, {}?",
+    "m_msg4": "Laugh, Laugh, Laugh,  {}"
+} #Coming Soon!!
