05bdbefdfdfd43baca1a98a1e4cf6151aefb318a
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-16 13:24:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import requests
 import time
 import asyncio
 from config import CONFIG
-from imgapi import SFW, NSFW
+from imgapi import SFW, NSFW, MEME
 from msg_channel import CHANNEL
+from custom_msg import W_MESSAGE, H_MESSAGE, B_MESSAGE, S_MESSAGE, M_MESSAGE
 
 client = commands.Bot(command_prefix=CONFIG['prefix'], intents = discord.Intents.all())
 
@@ -36,92 +37,94 @@ Logged in as {0.user}'''.format(client))
 @client.command()
 async def waifu(ctx, member : discord.Member=None):
     """Waifu Image for You"""
-    
-    async with ctx.typing():
-        if member is None:
-            member = ctx.author
-        if ctx.channel.is_nsfw():
-            await ctx.send('Note: Write this command outside the NSFW channel')
+    if member is None:
+        member = ctx.author
+    if ctx.channel.is_nsfw():
+        await ctx.send('Note: Write this command outside the NSFW channel')
             return
-        url = SFW['waifu1']
-        r = requests.get(url)
-        data = r.json()
-        img_url = data['url']
-        desc1 = [
-            f"Do you love me, {member.mention}?",
-            f"I love you, {member.mention}",
-            f"Do you love her, {member.mention}?",
-            f"Do you want her to be your girlfriend, {member.mention}?",
-            f"Please make me your bride, {member.mention}",
-            f"Take her to dinner, {member.mention}",
-            f"I will be your girlfriend, {member.mention}",
-            f"Hey, {member.mention}. Can you give me some love?",
-            f"Hey {member.mention}, you're my boyfriend right?",
-            f"She's your girlfriend right, {member.mention}?",
-        ]
-        embed = discord.Embed(
-            color=discord.Color.green(),
-            title="--- Random Waifu Image ---",
-            description=random.choice(desc1)
-        )
-        embed.set_image(url=img_url)
-        embed.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+    if ctx.channel.id is not int(CHANNEL['channel3']):
+        await ctx.send(f"Note: Write this command in {client.get_channel(int(CHANNEL['channel'])).mention}")
+        return
+    url = SFW['waifu1']
+    r = requests.get(url)
+    data = r.json()
+    img_url = data['url']
+    desc = [
+        str(W_MESSAGE['w_msg1']),
+        str(W_MESSAGE['w_msg2']),
+        str(W_MESSAGE['w_msg3']),
+        str(W_MESSAGE['w_msg4']),
+        str(W_MESSAGE['w_msg5']),
+        str(W_MESSAGE['w_msg6']),
+        str(W_MESSAGE['w_msg7']),
+        str(W_MESSAGE['w_msg8']),
+        str(W_MESSAGE['w_msg9']),
+        str(W_MESSAGE['w_msg10'])
+    ]
+    embed = discord.Embed(
+        color=discord.Color.green(),
+        title="--- Random Waifu Image ---",
+        description=random.choice(desc)
+    )
+    embed.set_image(url=img_url)
+    embed.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
         
-        await ctx.send(embed=embed)
+    await ctx.send(embed=embed)
 
 @client.command()
 async def hentai(ctx):
-    """Uncensored Anime Image (18+ Warning)"""
-    async with ctx.typing():
-        if ctx.channel.is_nsfw():
-            url1 = NSFW['hentai1']
-            url2 = NSFW['hentai2']
-            url3 = NSFW['hentai3']
-            author = ctx.message.author.mention
-            r1 = requests.get(url1)
-            r2 = requests.get(url2)
-            r3 = requests.get(url3)
-            data1 = r1.json()
-            data2 = r2.json()
-            data3 = r3.json()
-            img_url1 = data1['url']
-            img_url2 = data2['url']
-            img_url3 = data3['url']
-            imgdata = [
-                img_url1,
-                img_url2,
-                img_url3,
-            ]
-            titles = [
-                f"Are you satisfied, {author}",
-                f"You're horny right?, {author}",
-                f"I think, you're pervert {author}",
-                f"You got a good one, {author}"
-            ]
-            embed = discord.Embed(
-                title="--- 18+ Hentai Image ---",
-                description=random.choice(titles),
-            )
-            embed.set_image(url=random.choice(imgdata))
-            embed.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+    """Hentai Anime Image (18+ Warning)"""
+    if ctx.channel.is_nsfw():
+        url1 = NSFW['hentai1']
+        url2 = NSFW['hentai2']
+        url3 = NSFW['hentai3']
+        author = ctx.message.author.mention
+        r1 = requests.get(url1)
+        r2 = requests.get(url2)
+        r3 = requests.get(url3)
+        data1 = r1.json()
+        data2 = r2.json()
+        data3 = r3.json()
+        img_url1 = data1['url']
+        img_url2 = data2['url']
+        img_url3 = data3['url']
+        imgdata = [
+            img_url1,
+            img_url2,
+            img_url3,
+        ]
+        desc = [
+            str(H_MESSAGE['h_msg1']),
+            str(H_MESSAGE['h_msg2']),
+            str(H_MESSAGE['h_msg3']),
+            str(H_MESSAGE['h_msg4'])
+        ]
+        embed = discord.Embed(
+            color=discord.Color.green(),
+            title="--- 18+ Hentai Image ---",
+            description=random.choice(desc),
+        )
+        embed.set_image(url=random.choice(imgdata))
+        embed.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
             
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send('Note: Write this command in NSFW channel')
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send('Note: Write this command in NSFW channel')
 
 @client.command()
 async def slap(ctx, member : discord.Member=None):
+    """Slaps your friend or yourself"""
     if member is None:
         member = ctx.author
     url4 = SFW['slap1']
     r4 = requests.get(url4)
     data4 = r4.json()
     imgdata = data4['url']
-    author = ctx.message.author.mention
     desc = [
-        f"{author} slapped {member.mention}",
-        f"{author}: I got you, {member.mention}",
+        str(S_MESSAGE['s_msg1']),
+        str(S_MESSAGE['s_msg2'])
     ]
+    author = ctx.message.author.mention
     embed = discord.Embed(
         color=discord.Color.green(),
         title="--- Slap Someone ---",
@@ -129,6 +132,55 @@ async def slap(ctx, member : discord.Member=None):
     )
     embed.set_image(url=imgdata)
     embed.set_footer(text="Requested by {}".format(author), icon_url=ctx.message.author.avatar_url)
+    
+    await ctx.send(embed=embed)
+
+@client.command()
+async def bonk(ctx, member : discord.Member=None):
+    """Bonk your friends or yourself"""
+    if member is None:
+        member = ctx.author
+    url5 = SFW['bonk1']
+    r5 = requests.get(url5)
+    data5 = r5.json()
+    imgdata = data5['url']
+    desc = [
+        str(B_MESSAGE['b_msg1']),
+        str(B_MESSAGE['b_msg2']),
+        str(B_MESSAGE['b_msg3'])
+    ]
+    author = ctx.message.author.mention
+    embed = discord.Embed(
+        color=discord.Color.green(),
+        title="--- Bonk!! ---",
+        description=random.choice(desc)
+    )
+    embed.set_image(url=imgdata)
+    embed.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+        
+    await ctx.send(embed=embed)
+
+@client.command()
+async def meme(ctx):
+    """Random Meme Image"""
+    url6 = MEME['meme1']
+    r6 = requests.get(url6)
+    author = ctx.message.author.mention
+    data6 = r6.json()
+    imgdata = data6['url']
+    desc = [
+        str(M_MESSAGE['m_msg1']),
+        str(M_MESSAGE['m_msg2']),
+        str(M_MESSAGE['m_msg3']),
+        str(M_MESSAGE['m_msg4'])
+    ]
+    embed = discord.Embed(
+        color=discord.Color.green(),
+        title=data6['title'],
+        description=random.choice(desc)
+    )
+    embed.set_image(url=imgdata)
+    embed.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
     
     await ctx.send(embed=embed)
 
@@ -186,13 +238,14 @@ async def avatar_(ctx, avamem : discord.Member=None):
     if avamem is None:
         avamem = ctx.author
     useravatar = avamem.avatar_url
+    author = ctx.message.author.name
     embed = discord.Embed(
         color=discord.Color.green(),
         title="--- Avatar ---",
-        description=f"{avamem.mention} Profile Avatar",
+        description=f"{avamem.mention} Avatar",
     )
     embed.set_image(url=useravatar)
-    embed.set_footer(text="Requested by {}".format(ctx.message.author.name), icon_url=ctx.message.author.avatar_url)
+    embed.set_footer(text="Requested by {}".format(author), icon_url=ctx.message.author.avatar_url)
     
     await ctx.send(embed=embed)
 
