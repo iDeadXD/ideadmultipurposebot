5427085c92590c957b54c97a0849f32436a21e0c
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-17 10:54:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ CONFIG = {
     'prefix': '>',
     'token': 'OTA0MTU2MDI2ODUxNDU1MDA2.YX3a6w.5MpGgosOjwIYlz0iXFPSTVPwyqI',
     'twilio_sid': 'AC3bf04897b4874dd275de45ae8a653ac2',
-    'twilio_token': '235df4c6d26fa9c737cc75fb7efa0c00'
+    'twilio_token': '235df4c6d26fa9c737cc75fb7efa0c00',
+    'google_api_key': 'AIzaSyBN8VCOMcq3JtbCr6-IsZ5hV8A6oPIlFi8'
 }
