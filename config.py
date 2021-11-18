7142bf91bd408fb8f53ac94494c9d8f3d0828c87
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-18 18:49:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 CONFIG = {
-    'prefix': '>',
+    'prefix1': '>',
+    'prefix2': '.',
     'token': 'OTA0MTU2MDI2ODUxNDU1MDA2.YX3a6w.5MpGgosOjwIYlz0iXFPSTVPwyqI',
     'twilio_sid': 'AC3bf04897b4874dd275de45ae8a653ac2',
     'twilio_token': '235df4c6d26fa9c737cc75fb7efa0c00',
