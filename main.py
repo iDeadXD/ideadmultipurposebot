146712f7822829757a07c9d4880005751a1297ea
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-24 20:09:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pytz
 import requests
 import time
 import asyncio
+import pymongo
+from pymongo import MongoClient
 from config import CONFIG
 from imgapi import SFW, NSFW, MEME, WELCOME
 from msg_channel import CHANNEL
@@ -33,6 +35,53 @@ Logged in as {0.user}'''.format(client))
     ch2 = client.get_channel(int(CHANNEL['channel2']))
     await ch1.send(welcome + f" Bot Latency: {round(client.latency * 1000)}ms")
     await ch2.send(welcome + f" Bot Latency: {round(client.latency * 1000)}ms")
+
+@client.event
+async def on_message(ctx):
+    mango_url = "mongodb+srv://idead:idead@botdb.kqqpj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority_id"
+    cluster = MongoClient(mango_url)
+    db = cluster["databasel"]
+    collection = db["level"]
+    author_id = ctx.author.id
+    guild_id = ctx.guild.id 
+    
+    author = ctx.author
+    
+    user_id = {"_id": author_id}
+    
+    if ctx.author == client.user:
+        return
+    
+    if ctx.author.bot:
+  	return
+    
+    if(collection.count_documents({}) == 0):
+    	user_info = {"_id": author_id, "GuildID": guild_id, "Level": 1, "XP": 0}
+    	collection.insert_one(user_info)
+    
+    if(collection.count_documents(user_id) == 0):
+    	user_info = {"_id": author_id, "GuildID": guild_id, "Level": 1, "XP": 0}
+    	collection.insert_one(user_info)
+    
+    exp = collection.find(user_id)
+    for xp in exp:
+    	cur_xp = xp["XP"]
+    
+    	new_xp = cur_xp + 1 
+    
+    collection.update_one({"_id": author_id}, {"$set":{"XP":new_xp}}, upsert=True)
+    
+    #await ctx.channel.send("1 xp up")
+  
+    lvl = collection.find(user_id)
+    for levl in lvl:
+    	lvl_start = levl["Level"]
+    
+    	new_level = lvl_start + 1
+    
+    if cur_xp >= round(5 * (lvl_start ** 4 / 5)):
+    	collection.update_one({"_id": author_id}, {"$set":{"Level":new_level}}, upsert=True)
+    	await ctx.channel.send(f"{author.name} has leveled up to {new_level}!")
 
 @client.command()
 async def waifu(ctx, member : discord.Member=None):
