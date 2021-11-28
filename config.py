00066db34dd159106d3d342f71e63d3056fa5832
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-11-28 07:34:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 CONFIG = {
-    'prefix1': '>',
-    'prefix2': '>m',
-    'prefix3': '>v',
+    'default_prfx': '>',
+    'music_prfx': '>m',
+    'voice_prfx': '>v',
     'token': 'OTA0MTU2MDI2ODUxNDU1MDA2.YX3a6w.5MpGgosOjwIYlz0iXFPSTVPwyqI',
     'twilio_sid': 'AC3bf04897b4874dd275de45ae8a653ac2',
     'twilio_token': '235df4c6d26fa9c737cc75fb7efa0c00',
