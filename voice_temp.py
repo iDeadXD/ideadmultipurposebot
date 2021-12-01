d8adc6cd0bbf9fc4e9d9d8b5885014ac9a1fca66
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-12-01 08:31:00 +0700

    Update voice_temp.py

--- voice_temp.py
+++ voice_temp.py
@@ -26,7 +26,6 @@ class Voice(commands.Cog):
                         channel = discord.utils.get(guild.voice_channels, name=freq)
                         await member.move_to(channel)
                         return
-        return
         
         if len(before.channel.members) == 0:
             await before.channel.delete()
