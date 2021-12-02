8bd6c774707024d1536e3c0cfc02ba04a483cf24
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-12-02 09:36:44 +0700

    Create guild_utils.py

---
+++ guild_utils.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class Guilds(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command()
+    async def boostcount(self, ctx):
+        booster = len(ctx.guild.premium_subsctibers)
+        
+        boosted = discord.Embed(
+            title="--- Server Booster ---",
+            color=discord.Color.magenta()
+        )
+        boosted.set_thumbnail(url=ctx.guild.icon_url)
+        boosted.add_field(name="Booster Count", value=f"{str(ctx.guild.premium_subscription_count)}")
+        boosted.add_field(name="List of Booster", value=", ".join(booster.name))
+        boosted.set_footer(text="Thanks for boost this server")
+        
+        await ctx.send(embed=boosted)
+
+def setup(client):
+    client.add_cog(Guilds(client))
