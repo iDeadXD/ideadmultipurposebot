80c0ebcbe57c1fef3dbc324e8c36cc346aea9d48
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-12-04 11:05:19 +0700

    Update guild_utils.py

--- guild_utils.py
+++ guild_utils.py
@@ -29,44 +29,6 @@ class Guilds(commands.Cog):
         boosted.set_footer(text="Thanks for boost this server")
         
         await ctx.send(embed=boosted)
-    
-    @commands.command()
-    @commands.has_permissions(manage_guild=True)
-    async def prefix(self, ctx, prefixs=None):
-        if prefixs is None:
-            fail =discord.Embed(
-                title="",
-                description="Enter your prefix to change the default prefix. Default Prefix: >",
-                color=discord.Color.green()
-            )
-            await ctx.send(embed=fail)
-        
-        data = await self.client.collection.find(ctx.guild.id)
-        if data is None or "prefixes" not in data:
-            data = {"guild_id": ctx.guild.id, "_prefix": prefixs}
-        data["_prefix"] = prefixs
-        await self.client.collection.upsert(data)
-        
-        done = discord.Embed(
-            title="",
-            description=f"{self.client.user.mention}'s Prefix has been set to {prefixs}",
-            color=discord.Color.green()
-        )
-        
-        await ctx.send(embed=done)
-    
-    @commands.command()
-    @commands.has_permissions(manage_guild=True)
-    async def deleteprefix(self, ctx):
-        await self.client.collection.unset({"guild_id": ctx.guild.id, "_prefix": 1})
-        
-        done = discord.Embed(
-            title="",
-            description=f"{self.client.user.mention}'s Prefix has been set to default ( > )",
-            color=discord.Color.green()
-        )
-        
-        await ctx.send(embed=done)
 
 def setup(client):
     client.add_cog(Guilds(client))
