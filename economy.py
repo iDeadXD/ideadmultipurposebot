12638551ca71ad74b3d36710ac8e7843ac991dad
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-12-19 04:41:16 +0700

    Update economy.py

--- economy.py
+++ economy.py
@@ -231,7 +231,7 @@ class Economy(commands.Cog):
             )
             done.add_field(name="\u200b", value=f"Your Current Balance: {str(money_data)}")
             done.add_field(name="\u200b", value=f"Last Activity: {str(curr_status)}")
-            print(curr_data)
+            print(money_data)
             await ctx.send(embed=done)
 
 def setup(client):
