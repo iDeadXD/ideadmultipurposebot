8277da397d070c4d0c3eeb0f78d9841142686610
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2021-12-31 19:23:05 +0700

    Update dev.py

--- dev.py
+++ dev.py
@@ -25,9 +25,6 @@ class Developer(commands.Cog):
             
             if passw.content == 'idead1511':
                 await passw.delete()
-                for guild in self.client.guilds:
-                    for owner in len(guild.owner):
-                        await owner.send("I'm Offline Now (This message is automatically sent when the bot is shutting down.)")
                 await asyncio.sleep(1)
                 await ctx.send("Shutting Down...")
                 await asyncio.sleep(3)
