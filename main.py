33597f77486d710cf1487cfc5d57c1eb5eb1d320
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-01-01 09:14:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -120,6 +120,20 @@ async def on_ready():
     print(f'[*] Loaded Cogs: {curr_cogs}')
     print(f'[*] Serving on: {curr_server} Server')
 
+@client.event #on_message
+async def on_message(message):
+    hello_m = ["halo", "hello", "hola"]
+    
+    for msg in hello_m: #Check if message content in hello_m
+        if message.content.lower().startswith(str(msg)):
+            halo = discord.Embed(
+                title="",
+                description=f"Halo juga {message.author.mention}, Semoga Hari mu Menyenangkan.",
+                color=discord.Color.purple()
+            )
+            await message.reply(embed=halo)
+    await client.process_commands(message)
+
 @client.event
 async def on_member_join(member):
     main_ch = member.guild.system_channel
