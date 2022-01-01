b34a56767d571f557c9b845a44c3a3080bb69afb
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-01-01 08:58:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,5 @@ CONFIG = {
     'default_prfx': '>',
     'music_prfx': '>m',
     'voice_prfx': '>v',
-    'token': 'OTA0MTU2MDI2ODUxNDU1MDA2.YX3a6w.H047TKD2T8B7_C3BGshJdUXNIZ8',
-    'twilio_sid': 'AC3bf04897b4874dd275de45ae8a653ac2',
-    'twilio_token': '235df4c6d26fa9c737cc75fb7efa0c00',
-    'google_api_key': 'AIzaSyBN8VCOMcq3JtbCr6-IsZ5hV8A6oPIlFi8',
     'mongodb_url': 'mongodb+srv://idead:idead@botdb.kqqpj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
