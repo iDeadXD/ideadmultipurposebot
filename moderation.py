751595c43ebfe406b2fad451a0bed7c8cbedff4f
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-01-04 17:19:05 +0700

    Update moderation.py

--- moderation.py
+++ moderation.py
@@ -35,123 +35,148 @@ class Moderator(commands.Cog):
         await ctx.message.author.edit(nick=str(names))
     
     @commands.command()
+    @commands.has_permissions(manage_guild=True, ban_members=True, kick_members=True)
     async def ban(self, ctx, member : discord.Member=None, *, reason=None):
-        yes = ["y", "yes", "Y"]
-        no = ["n", "no", "N"]
-        
-        if ctx.message.author is not ctx.guild.owner:
-            await ctx.send("You're not Owner in this Server. Command Ignored")
-            return
-        if reason is None:
-            await ctx.send("Reason required!!")
-            return
-        if member is ctx.guild.owner and member != None:
-            await ctx.send("Owner!!. You can't ban Server Owner")
-            return
-        if member is None:
-            await ctx.send("You have to choose a member to get banned. Command Ignored")
-            return
-        
-        confirm = discord.Embed(
-            title="",
-            description=f"Are you sure you want to ban {member.mention}? (y/n)",
-            color=discord.Color.magenta()
-        )
-        
-        await ctx.send(embed=confirm)
-        msg = await self.client.wait_for('message', check=lambda message : message.author == ctx.author and message.channel == ctx.channel)
-        if msg.content in yes:
-            guild = ctx.guild.name
-            await member.ban()
-            embed = discord.Embed(
-                title="--- Banned Member ---",
-                color=discord.Color.red()
-            )
-            embed.add_field(name="Member Name", value=f"{member.mention}")
-            embed.add_field(name="Punishment", value="Banned from Server")
-            embed.add_field(name="Reason", value=f"{reason}")
-            embed.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
-            embed.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+        try:
+            yes = ["y", "yes", "Y"]
+            no = ["n", "no", "N"]
             
-            banned = discord.Embed(
-                title="--- You have been Banned ---",
-                color=discord.Color.red()
-            )
-            banned.add_field(name="Punishment", value="Banned from Server")
-            banned.add_field(name="Reason", value=f"{reason}")
-            banned.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
-            banned.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+            if reason is None:
+                await ctx.send("Reason required!!")
+                return
+            if member is ctx.guild.owner and member != None:
+                await ctx.send("Owner!!. You can't ban Server Owner")
+                return
+            if member is None:
+                await ctx.send("You have to choose a member to get banned. Command Ignored")
+                return
             
-            await ctx.send(embed=embed)
-            await ctx.send("✔ User has been notified.")
-            await member.send(embed=banned)
-        
-        elif msg.content in no:
-            cancelled = discord.Embed(
+            confirm = discord.Embed(
                 title="",
-                description=f"Ban {member.mention} cancelled",
-                color=discord.Color.green()
+                description=f"Are you sure you want to ban {member.mention}? (y/n)",
+                color=discord.Color.magenta()
             )
             
-            await ctx.send(embed=cancelled)
+            await ctx.send(embed=confirm)
+            msg = await self.client.wait_for('message', check=lambda message : message.author == ctx.author and message.channel == ctx.channel)
+            if msg.content in yes:
+                guild = ctx.guild.name
+                await member.ban()
+                embed = discord.Embed(
+                    title="--- Banned Member ---",
+                    color=discord.Color.red()
+                )
+                embed.add_field(name="Member Name", value=f"{member.mention}")
+                embed.add_field(name="Punishment", value="Banned from Server")
+                embed.add_field(name="Reason", value=f"{reason}")
+                embed.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
+                embed.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+                
+                banned = discord.Embed(
+                    title="--- You have been Banned ---",
+                    color=discord.Color.red()
+                )
+                banned.add_field(name="Punishment", value="Banned from Server")
+                banned.add_field(name="Reason", value=f"{reason}")
+                banned.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
+                banned.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+                
+                await ctx.send(embed=embed)
+                await ctx.send("✔ User has been notified.")
+                await member.send(embed=banned)
+            
+            elif msg.content in no:
+                cancelled = discord.Embed(
+                    title="",
+                    description=f"Ban {member.mention} cancelled",
+                    color=discord.Color.green()
+                )
+                
+                await ctx.send(embed=cancelled)
+        except asyncio.TimeoutError:
+            failed = discord.Embed(
+                title="",
+                description="Ban Timeout",
+                color=discord.Color.red()
+            )
+            return await ctx.send(embed=failed)
+    
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You are missing permission(s) to run this command.")
+        else:
+            raise error
     
     @commands.command()
+    @commands.has_permissions(manage_guild=True, kick_members=True)
     async def kick(self, ctx, member : discord.Member=None, *, reason=None):
-        yes = ["y", "yes", "Y"]
-        no = ["n", "no", "N"]
-        
-        if ctx.message.author is not ctx.guild.owner:
-            await ctx.send("You're not Owner in this Server. Command Ignored")
-            return
-        if reason is None:
-            await ctx.send("Reason required!!")
-            return
-        if member is ctx.guild.owner and member != None:
-            await ctx.send("Owner!!. You can't kick Server Owner")
-            return
-        
-        confirm = discord.Embed(
-            title="",
-            description=f"Are you sure you want to kick {member.mention}? (y/n)",
-            color=discord.Color.magenta()
-        )
-        
-        await ctx.send(embed=confirm)
-        msg = await self.client.wait_for('message', check=lambda message : message.author == ctx.author and message.channel == ctx.channel)
-        if msg.content in yes:
-            guild = ctx.guild.name
-            await member.kick()
-            embed = discord.Embed(
-                title="--- Kicked Member ---",
-                color=discord.Color.red()
-            )
-            embed.add_field(name="Member Name", value=f"{member.mention}")
-            embed.add_field(name="Punishment", value="Kicked from Server")
-            embed.add_field(name="Reason", value=f"{reason}")
-            embed.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
-            embed.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+        try:
+            yes = ["y", "yes", "Y"]
+            no = ["n", "no", "N"]
             
-            kicked = discord.Embed(
-                title="--- You have been Kicked ---",
-                color=discord.Color.red()
-            )
-            kicked.add_field(name="Punishment", value="Kicked from Server")
-            kicked.add_field(name="Reason", value=f"{reason}")
-            kicked.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
-            kicked.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+            if reason is None:
+                await ctx.send("Reason required!!")
+                return
+            if member is ctx.guild.owner and member != None:
+                await ctx.send("Owner!!. You can't kick Server Owner")
+                return
             
-            await ctx.send(embed=embed)
-            await ctx.send("✔ User has been notified.")
-            await member.send(embed=kicked)
-        
-        elif msg.content in no:
-            cancelled = discord.Embed(
+            confirm = discord.Embed(
                 title="",
-                description=f"Kick {member.mention} cancelled",
-                color=discord.Color.green()
+                description=f"Are you sure you want to kick {member.mention}? (y/n)",
+                color=discord.Color.magenta()
             )
             
-            await ctx.send(embed=cancelled)
+            await ctx.send(embed=confirm)
+            msg = await self.client.wait_for('message', check=lambda message : message.author == ctx.author and message.channel == ctx.channel)
+            if msg.content in yes:
+                guild = ctx.guild.name
+                await member.kick()
+                embed = discord.Embed(
+                    title="--- Kicked Member ---",
+                    color=discord.Color.red()
+                )
+                embed.add_field(name="Member Name", value=f"{member.mention}")
+                embed.add_field(name="Punishment", value="Kicked from Server")
+                embed.add_field(name="Reason", value=f"{reason}")
+                embed.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
+                embed.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+                
+                kicked = discord.Embed(
+                    title="--- You have been Kicked ---",
+                    color=discord.Color.red()
+                )
+                kicked.add_field(name="Punishment", value="Kicked from Server")
+                kicked.add_field(name="Reason", value=f"{reason}")
+                kicked.add_field(name="Moderator", value=f"{ctx.message.author.mention}")
+                kicked.set_footer(text="Today at {}".format(datetime.now(pytz.timezone('Asia/Jakarta')).strftime("%H:%M:%S")))
+                
+                await ctx.send(embed=embed)
+                await ctx.send("✔ User has been notified.")
+                await member.send(embed=kicked)
+            
+            elif msg.content in no:
+                cancelled = discord.Embed(
+                    title="",
+                    description=f"Kick {member.mention} cancelled",
+                    color=discord.Color.green()
+                )
+                
+                await ctx.send(embed=cancelled)
+        except asyncio.TimeoutError:
+            failed = discord.Embed(
+                title="",
+                description="Kick Timeout!!"
+            )
+            return await ctx.send(embed=failed)
+    
+    @kick.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, MissingPermissions):
+            await ctx.send("You are missing permission(s) to run this command.")
+        else:
+            raise error
 
 def setup(client):
     client.add_cog(Moderator(client))
