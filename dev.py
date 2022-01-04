89698ddc00c3cd0c7e2e72551e26976d2deab313
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-01-04 20:05:11 +0700

    Update dev.py

--- dev.py
+++ dev.py
@@ -19,15 +19,16 @@ class Developer(commands.Cog):
                 description="Are you sure want shutdown me? (Enter Password)",
                 color=ctx.author.color
             )
-            await ctx.send(embed=getpass)
+            sendconfirm = await ctx.send(embed=getpass)
             
             passw = await self.client.wait_for('message', check=lambda message:message.author == ctx.author and message.channel == ctx.channel, timeout=10)
             
             if passw.content == 'idead1511':
+                await sendconfirm.delete()
                 await passw.delete()
                 await asyncio.sleep(1)
                 await ctx.send("Shutting Down...")
-                await asyncio.sleep(3)
+                await asyncio.sleep(4)
                 await ctx.send("Shutdown Completed.")
                 await asyncio.sleep(1)
                 await self.client.close()
