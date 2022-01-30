906b746bfec2d7bc54035b6eb010389bb39d3dda
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-01-30 15:24:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -152,8 +152,7 @@ async def on_message(message):
     dev = client.get_guild(message.guild.id).get_member(843132313562513408)
     
     for msg in hello_m: #Check if message content in hello_m
-        if message.guild.id == 836464932236165140:
-            return
+        
         if message.content.lower().startswith(msg):
             halo = discord.Embed(
                 title="",
