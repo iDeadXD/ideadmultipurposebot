0a506f563d747d2e1b2db5a54ee45321fd06be2e
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-02-14 11:58:27 +0700

    Update menupages.py

--- menupages.py
+++ menupages.py
@@ -1,51 +1,28 @@
 import discord
-from discord import ui
 from discord.ext import menus
 
-class MyMenuPages(ui.View, menus.MenuPages):
-    def __init__(self, source, *, delete_message_after=False):
-        super().__init__(timeout=60)
-        self._source = source
-        self.current_page = 0
-        self.ctx = None
-        self.message = None
-        self.delete_message_after = delete_message_after
-
-    async def start(self, ctx, *, channel=None, wait=False):
-        # We wont be using wait/channel, you can implement them yourself. This is to match the MenuPages signature.
-        await self._source._prepare_once()
-        self.ctx = ctx
-        self.message = await self.send_initial_message(ctx, ctx.channel)
-
-    async def _get_kwargs_from_page(self, page):
-        """This method calls ListPageSource.format_page class"""
-        value = await super()._get_kwargs_from_page(page)
-        if 'view' not in value:
-            value.update({'view': self})
-        return value
-
-    async def interaction_check(self, interaction):
-        """Only allow the author that invoke the command to be able to use the interaction"""
-        return interaction.user == self.ctx.author
-
-    @ui.button(emoji='<:before_fast_check:754948796139569224>', style=discord.ButtonStyle.blurple)
-    async def first_page(self, button, interaction):
-        await self.show_page(0)
-
-    @ui.button(emoji='<:before_check:754948796487565332>', style=discord.ButtonStyle.blurple)
-    async def before_page(self, button, interaction):
+class MyMenuPages(menus.MenuPages, inherit_buttons=False):
+    @menus.button("<:before_check:754948796487565332>", position=menus.First(1))
+    async def go_before(self, payload):
+        """Goes to the previous page."""
         await self.show_checked_page(self.current_page - 1)
 
-    @ui.button(emoji='<:stop_check:754948796365930517>', style=discord.ButtonStyle.blurple)
-    async def stop_page(self, button, interaction):
-        self.stop()
-        if self.delete_message_after:
-            await self.message.delete(delay=0)
-
-    @ui.button(emoji='<:next_check:754948796361736213>', style=discord.ButtonStyle.blurple)
-    async def next_page(self, button, interaction):
+    @menus.button("<:next_check:754948796361736213>", position=menus.Last(0))
+    async def go_after(self, payload):
+        """Goes to the next page."""
         await self.show_checked_page(self.current_page + 1)
 
-    @ui.button(emoji='<:next_fast_check:754948796391227442>', style=discord.ButtonStyle.blurple)
-    async def last_page(self, button, interaction):
+    @menus.button("<:before_fast_check:754948796139569224>", position=menus.First(0))
+    async def go_first(self, payload):
+        """Goes to the first page."""
+        await self.show_page(0)
+
+    @menus.button("<:next_fast_check:754948796391227442>", position=menus.Last(1))
+    async def go_last(self, payload):
+        """Goes to the last page."""
         await self.show_page(self._source.get_max_pages() - 1)
+    
+    @menus.button("<:stop_check:754948796365930517>", position=menus.First(2))
+    async def go_stop(self, payload):
+        """Remove this message."""
+        self.stop()
