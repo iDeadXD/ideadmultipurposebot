1d087dd6714cab54c7d1649d9beeb4d6901c1951
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-03-29 13:40:19 +0700

    Update music.py

--- music.py
+++ music.py
@@ -18,7 +18,7 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 ytdlopts = {
     'format': 'bestaudio/best',
     'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'buffersize': 48000,
+    'buffersize': 54000,
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
