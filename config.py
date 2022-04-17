0242069894e6f43b7efbe6ac5e5a47dd53d4f047
Rafi Ahmad Firdaus <67831149+iDeadXD@users.noreply.github.com>
2022-04-17 15:42:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+import os
 CONFIG = {
     'default_prfx': '>',
     'music_prfx': '>m',
     'voice_prfx': '>v',
-    'mongodb_url': 'mongodb+srv://idead:1981@botdb.kqqpj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    'mongodb_url': os.getenv('MONGODB_URL')
 }
